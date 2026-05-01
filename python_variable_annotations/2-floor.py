ad0deba271a24a2f103bc23b49d8cf483a19dc61
Fidan Umudova <fidanumudova05@gmail.com>
2026-05-01 12:17:01 +0400

    Create 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for floor function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float as an integer"""
+    return math.floor(n)
