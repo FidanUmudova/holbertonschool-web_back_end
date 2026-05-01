c47be29d326c2ab8f5ad59713fcb6fe761c15223
Fidan Umudova <fidanumudova05@gmail.com>
2026-05-01 12:50:25 +0400

    Update 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,7 +9,19 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Run async_comprehension 4 times in parallel and measure runtime."""
-    start = time.perf_counter()
+    start = time.perf_counter()#!/usr/bin/env python3
+"""Async generator module."""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield 10 random numbers between 0 and 10."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
 
     await asyncio.gather(
         async_comprehension(),
