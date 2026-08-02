9261b4ea888e6b2e717a5a177310f85cb59f0a6f
Fidan Umudova <fidanumudova05@gmail.com>
2026-08-02 10:43:55 +0400

    Create 3-get_ids_sum.js

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(listStudents) {
+  if (!Array.isArray(listStudents)) {
+    return 0;
+  }
+  return listStudents.reduce((accumulator, student) => accumulator + student.id, 0);
+}
