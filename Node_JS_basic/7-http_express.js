4f5d5a2b751c92479efca821385744624340128d
Fidan Umudova <fidanumudova05@gmail.com>
2026-08-09 16:11:02 +0400

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,52 +2,50 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-
 const database = process.argv[2];
 
 app.get('/', (req, res) => {
-  res.type('text').send('Hello Holberton School!');
+res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  fs.readFile(database, 'utf8', (err, data) => {
-    if (err) {
-      res
-        .type('text')
-        .status(500)
-        .send('Cannot load the database');
-      return;
-    }
+fs.readFile(database, 'utf8', (err, data) => {
+if (err) {
+res.status(500).send('Cannot load the database');
+return;
+}
 
-    const lines = data
-      .split('\n')
-      .filter((line) => line.trim() !== '');
+```
+const lines = data
+  .trim()
+  .split('\n');
 
-    const students = lines.slice(1);
+const students = lines.slice(1);
+const fields = {};
 
-    const fields = {};
+students.forEach((student) => {
+  const studentInfo = student.split(',');
+  const firstName = studentInfo[0];
+  const field = studentInfo[3];
 
-    students.forEach((line) => {
-      const student = line.split(',');
-      const firstName = student[0];
-      const field = student[student.length - 1].trim();
+  if (!fields[field]) {
+    fields[field] = [];
+  }
 
-      if (!fields[field]) {
-        fields[field] = [];
-      }
+  fields[field].push(firstName);
+});
 
-      fields[field].push(firstName);
-    });
+let result = 'This is the list of our students\n';
+result += `Number of students: ${students.length}\n`;
 
-    let output = 'This is the list of our students\n';
-    output += `Number of students: ${students.length}\n`;
+Object.keys(fields).forEach((field) => {
+  result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+});
 
-    Object.keys(fields).forEach((field) => {
-      output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-    });
+res.send(result);
+```
 
-    res.type('text').send(output.trim());
-  });
+});
 });
 
 app.listen(1245);
