ebdba2009c59092a2bb380b377e485341d0cce91
Fidan Umudova <fidanumudova05@gmail.com>
2026-08-09 16:45:04 +0400

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,17 +4,44 @@ const fs = require('fs');
 const app = express();
 const port = 1245;
 
+// countStudents funksiyasını birbaşa 3-cü tapşırıqdakı kimi yazırıq ki, kənar fayla toxunulmasın
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
-      }const express = require('express');
-const countStudents = require('./3-read_file_async');
+      }
 
-const app = express();
-const port = 1245;
+      const students = data
+        .trim()
+        .split('\n')
+        .slice(1);
+
+      const fields = {};
+
+      students.forEach((student) => {
+        const studentInfo = student.split(',');
+        const firstName = studentInfo[0];
+        const field = studentInfo[studentInfo.length - 1];
+
+        if (firstName && field) {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstName);
+        }
+      });
+
+      let output = `Number of students: ${students.length}`;
+      Object.keys(fields).forEach((field) => {
+        output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+      });
+
+      resolve(output);
+    });
+  });
+}
 
 // Route for root /
 app.get('/', (req, res) => {
@@ -35,63 +62,6 @@ app.get('/students', async (req, res) => {
 });
 
 // Start listening on port 1245
-app.listen(port);
-
-// Export the app variable
-module.exports = app;
-
-      const lines = data.split('\n');
-      const validLines = lines.filter((line) => line.trim() !== '');
-
-      if (validLines.length <= 1) {
-        resolve('Number of students: 0');
-        return;
-      }
-
-      const students = validLines.slice(1);
-      const totalMsg = `Number of students: ${students.length}`;
-
-      const fields = {};
-
-      students.forEach((student) => {
-        const studentInfo = student.split(',');
-        const firstName = studentInfo[0];
-        const field = studentInfo[studentInfo.length - 1];
-
-        if (firstName && field) {
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstName);
-        }
-      });
-
-      let output = [totalMsg];
-      Object.keys(fields).forEach((field) => {
-        const fieldMsg = `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
-        output.push(fieldMsg);
-      });
-
-      resolve(output.join('\n'));
-    });
-  });
-}
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', async (req, res) => {
-  const dbFile = process.argv[2];
-  
-  try {
-    const studentData = await countStudents(dbFile);
-    res.send(`This is the list of our students\n${studentData}`);
-  } catch (error) {
-    res.send('This is the list of our students\nCannot load the database');
-  }
-});
-
 app.listen(port);
 
 module.exports = app;
